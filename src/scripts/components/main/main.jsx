c12d4801bc0d186b1221623b70d3ccd2777fef84
VyglD <VyglD@yandex.ru>
2021-03-06 17:36:13 +0300

    Добален компонент `slider`

--- src/scripts/components/main/main.jsx
+++ src/scripts/components/main/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Slider from "../slider/slider";
 
 const properties = [
   {
@@ -25,39 +26,44 @@ const Main = (props) => {
 
   return (
     <main className="prime-main">
-      <div className="prime-main__content">
-        <h1 className="prime-main__title">Марпех 11</h1>
-        <ul className="prime-main__properties-list">
-          {
-            properties.map((property, index) => (
-              <li
-                key={index}
-                className={
-                  `prime-main__property
-                  prime-main__property--${property.modifier}`
-                }
-              >
-                {property.title}
-              </li>
-            ))
-          }
-        </ul>
-        <p className="prime-main__price">2 300 000 ₽</p>
-        <p className="prime-main__price prime-main__price--old">2 400 000 ₽</p>
-        <a
-          className="prime-main__purchase"
-          href="#blank"
-          onClick={onBlankLinkClick}
-        >
-          Оставить заявку
-        </a>
-        <a
-          className="prime-main__credit"
-          href="#blank"
-          onClick={onBlankLinkClick}
-        >
-          В кредит от 11 000 ₽
-        </a>
+      <div className="prime-main__banner">
+        <div className="prime-main__right-column">
+          <h1 className="prime-main__title">Марпех 11</h1>
+          <ul className="prime-main__properties-list">
+            {
+              properties.map((property, index) => (
+                <li
+                  key={index}
+                  className={
+                    `prime-main__property
+                    prime-main__property--${property.modifier}`
+                  }
+                >
+                  {property.title}
+                </li>
+              ))
+            }
+          </ul>
+          <p className="prime-main__price">2 300 000 ₽</p>
+          <p className="prime-main__price prime-main__price--old">2 400 000 ₽</p>
+          <a
+            className="prime-main__purchase"
+            href="#blank"
+            onClick={onBlankLinkClick}
+          >
+            Оставить заявку
+          </a>
+          <a
+            className="prime-main__credit"
+            href="#blank"
+            onClick={onBlankLinkClick}
+          >
+            В кредит от 11 000 ₽
+          </a>
+        </div>
+        <div className="prime-main__left-column">
+          <Slider />
+        </div>
       </div>
     </main>
   );
