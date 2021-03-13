2de879fa973f9aa36189695bd02a0b4289501f00
VyglD <VyglD@yandex.ru>
2021-03-13 23:11:23 +0300

    Добавлен адаптив для компонента `contacts`

--- src/scripts/components/contacts/contacts.jsx
+++ src/scripts/components/contacts/contacts.jsx
@@ -6,16 +6,30 @@ const Contacts = () => {
     <section className="contacts">
       <h3 className="contacts__title visually-hidden">Контакты</h3>
       <div className="contacts__info-wrapper">
-        <p className="contacts__address-title">Адрес</p>
-        <p className="contacts__address">
+        <p className="contacts__info-title contacts__info-title--address">
+          Адрес
+        </p>
+        <p className="contacts__info-content contacts__info-content--address">
           Санкт-Петербург, <br/> набережная реки Карповки, дом 5
         </p>
-        <p className="contacts__time-title">Режим работы</p>
-        <p className="contacts__time">Ежедневно, с 10:00 до 21:00</p>
-        <p className="contacts__phone-title">Телефон</p>
-        <p className="contacts__phone">8 (800) 333-55-99</p>
-        <p className="contacts__email-title">E-mail</p>
-        <p className="contacts__email">info@avto-moto.ru</p>
+        <p className="contacts__info-title contacts__info-title--time">
+          Режим работы
+        </p>
+        <p className="contacts__info-content contacts__info-content--time">
+          Ежедневно, с 10:00 до 21:00
+        </p>
+        <p className="contacts__info-title contacts__info-title--phone">
+          Телефон
+        </p>
+        <p className="contacts__info-content contacts__info-content--phone">
+          8 (800) 333-55-99
+        </p>
+        <p className="contacts__info-title contacts__info-title--email">
+          E-mail
+        </p>
+        <p className="contacts__info-content contacts__info-content--email">
+          info@avto-moto.ru
+        </p>
       </div>
       <div className="contacts__map-wrapper">
         <Map />
